28a8c65579fd77a6f461216a7eb3483260000edc
Aidan Coyle <packrat386@gmail.com>
2015-05-27 10:31:16 -0500

    Added cuke logic for checking bomb state

--- test_eng/overlord/features/activation.feature
+++ test_eng/overlord/features/activation.feature
@@ -15,15 +15,15 @@ Feature: Bomb can be activated
     
   Scenario: Activating Bomb
     Given the bomb has been booted with activation code 1234
-    When I enter code 1234 into the bomb
+    When I activate the bomb with code 1234
     Then the bomb will be activated
 
   Scenario: Wrong Code
     Given the bomb has been booted with activation code 1234
-    When I enter code 5678 into the bomb
+    When I activate the bomb with code 5678
     Then the bomb will not be activated
 
   Scenario: Already Active
     Given the bomb has been activated with code 1234
-    When I enter code 1234 into the bomb
+    When I activate the bomb with code 12334
     Then the activation status will not change
--- test_eng/overlord/features/step_definitions/activation_steps.rb
+++ test_eng/overlord/features/step_definitions/activation_steps.rb
@@ -12,42 +12,62 @@ def get_data(url)
   data = JSON.parse(browser.last_response.body)  
 end
 
-Given(/^the bomb is not booted$/) do
+def reset_bomb
   data = get_data('/api/reset')
-  expect(data["success"]).to be_truthy
-  expect(data["state"]).to eq("unset")
+  expect(data['success']).to be_truthy
+  expect(data['state']).to eq('unset')
+end
+
+def boot_bomb(acode)
+  data = get_data("/api/boot?activation_code=#{acode}")
+  expect(data['success']).to be_truthy
+  expect(data['state']).to eq('booted')
+end
+
+def activate_bomb(acode)
+  data = get_data("/api/activate?activation_code=#{acode}")
+  expect(data['success']).to be_truthy
+  expect(data['state']).to eq('active')
+end
+
+def check_state(state)
+  data = get_data('/api/state/')
+  expect(data['state']).to eq(state)
+end
+
+Given(/^the bomb is not booted$/) do
+  reset_bomb
 end
 
 When(/^I boot the bomb with activation code (\d+)$/) do |acode|
-  data = get_data("/api/boot?activation_code=#{acode}")
-  expect(data["success"]).to be_truthy
-  expect(data["state"]).to eq("booted")
+  boot_bomb(acode)
 end
 
 Then(/^The bomb will activate with code (\d+)$/) do |acode|
-  data = get_data("/api/activate?activation_code=#{acode}")
-  expect(data["success"]).to be_truthy
-  expect(data["state"]).to eq("active")
+  activate_bomb(acode)
 end
 
 When(/^I boot the bomb with no activation code$/) do
-
+  data = get_data('/api/boot')
+  expect(data['success']).to be_truthy
+  expect(data['state']).to eq('booted')  
 end
 
-Given(/^the bomb has been booted with activation code (\d+)$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^the bomb has been booted with activation code (\d+)$/) do |acode|
+  reset_bomb
+  boot_bomb(acode)
 end
 
-When(/^I enter code (\d+) into the bomb$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I activate the bomb with code (\d+)$/) do |acode|
+  activate_bomb(acode)
 end
 
 Then(/^the bomb will be activated$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  check_state('active')
 end
 
 Then(/^the bomb will not be activated$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  check_state('booted')
 end
 
 Given(/^the bomb has been activated with code (\d+)$/) do |arg1|
